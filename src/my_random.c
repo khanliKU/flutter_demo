08fc4150d3b3f4186c008fb388c64180fdd6bdac
khanliKU <kutlay.hanli@maximintegrated.com>
2022-11-17 15:33:25 +0300

    added my_random to windows

--- src/my_random.c
+++ src/my_random.c
@@ -1,11 +1,16 @@
-#include "my_random.h"
+#include "./my_random.h"
 #include <stdlib.h>
+#include <limits.h>
 
 uint myRandom(uint max){
-    uint random = 0;
+    uint random = 1;
+    if (max == 0){
+        max = UINT_MAX;
+    }
     for (size_t i = 0; i <= (max/RAND_MAX); i++)
     {
-        random += rand();
+        random *= rand();
+        i <<= 15;
     }
     return random % max;
 }
